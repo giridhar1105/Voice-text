7ec60881f05cd5576062ae4e60b22030489e1c95
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-11-30 00:27:58 +0530

    Added macha lang parse tamil :zap:

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const axios = require('axios'); 
 const {jsToMachaLangKannada} = require('./jstomacha')
 const {jsToMachaLangTelugu} = require('./jstomachatelugu')
-const {jsToMachaLangTelugu} = require('./jstomacha')
+const {jsToMachaLangTamil} = require('./jstomachatamil')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,26 @@ const Prompt = "Provide the JavaScript code only, without any markdown, formatti
 
   try {
     const geminiApiResponse = await processWithGeminiTelugu(Prompt,input);
+
+    res.status(200).json({
+      input,
+      aiResponse: geminiApiResponse,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error processing the request' });
+  }
+});
+app.post('/macha_tamil', async (req, res) => {
+  const { input } = req.body;
+const Prompt = "Provide the JavaScript code only, without any markdown, formatting tags, or comments. Use objects instead of classes and procedural programming techniques exclusively. Strictly avoid using 'classes', 'function constructor' , 'var' keyword in the solution, make sure no where in the code 'this' keyword is used, if you cant return a code without satisfying the constraints mentioned above don't give the code. You have the ability to use JavaScript object. Return only the raw code."
+;
+  if (!input || typeof input !== 'string') {
+    return res.status(400).json({ error: 'Invalid input text' });
+  }
+
+  try {
+    const geminiApiResponse = await processWithGeminiTamil(Prompt,input);
 
     res.status(200).json({
       input,
@@ -92,6 +112,28 @@ async function processWithGeminiKannada(Prompt , input) {
       throw new Error('Failed to process the request');
     }
   }
+
+  async function processWithGeminiTamil(Prompt , input) {
+    try {
+      const response = await axios({
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCWFTmjf8m7nlCdvrDZ20w1TvBDU7YpJKE`,
+        method: "post",
+        data: {
+          contents: [{ parts: [{ text: Prompt + input }] }],
+        },
+      });
+ const aiResponse = response.data.candidates[0].content.parts[0].text;
+ const parsedText =  jsToMachaLangTamil(aiResponse) 
+ console.log(parsedText)
+ console.log(aiResponse)
+      return parsedText;
+    } catch (error) {
+      console.error('Error in calling Gemini 1.5 Flash API:', error);
+      console.error('Server Response:', error.response.data); 
+      throw new Error('Failed to process the request');
+    }
+  }
+
 
 
 app.listen(port, () => {
